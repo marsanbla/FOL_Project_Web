dfcffc96d1c3b311d206f4da06a34f368dde3d6e
a21marsanbla <a21marsanbla@inspedralbes.cat>
2023-05-08 09:51:38 +0200

    tm

--- src/views/TemasLibro.vue
+++ src/views/TemasLibro.vue
@@ -1,38 +1,40 @@
-<template>
-        <div class="test">
 
-        <div class="text-center">
-            <button class="primary" @click="currentPage = currentPage > 1 ? currentPage - 1 : 1">Prev</button>
-              <div>{{ currentPage }} / {{ pages }}</div>
-            <button class="primary" @click="currentPage = currentPage < pages ? currentPage + 1 : pages">Next</button>
-          </div>
-          <div class="text-center">
-            <VuePDF :pdf="pdf" :page="currentPage" />
-          </div>
 
-        </div>
-    
-</template>
-<script>
-/*import { usePDF, VuePDF } from "@tato30/vue-pdf";
+/*<template>
+<button class="expansion-panel">View PDF</button>
 
-export default {
-  components: {
-    VuePDF,
-  },
-  setup() {
-    const currentPage = ref(1);
-    const { pdf, pages } = usePDF("https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf");
-        return {
-      pdf,
-      pages,
-      currentPage
-    };
-  },
-};*/
+<div id="pdf-container"></div>
+</template>*/
+
+/*<script>
+const button = document.querySelector('.expansion-panel');
+const container = document.querySelector('#pdf-container');
+
+button.addEventListener('click', () => {
+  const url = 'http://oatpfe.es/docs/repositorio/es_ES/manuales/formacion_y_orientacion_laboral.pdf';
+
+  // Load PDF file using PDF.js
+  PDFJS.getDocument(url).then(pdf => {
+    // Display PDF in container element
+    for (let i = 1; i <= pdf.numPages; i++) {
+      pdf.getPage(i).then(page => {
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        const viewport = page.getViewport({ scale: 1 });
+        const scale = container.offsetWidth / viewport.width;
+        canvas.width = container.offsetWidth;
+        canvas.height = viewport.height * scale;
+        const renderContext = {
+          canvasContext: context,
+          viewport: page.getViewport({ scale })
+        };
+        page.render(renderContext).then(() => {
+          container.appendChild(canvas);
+        });
+      });
+    }
+  });
+});
 </script>
-<style>
-.test{
-margin-top: 200px;
-}
-</style>
+
+*/
